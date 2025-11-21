62c65a98e1bc1048cdaf87b5e5147472aa22cfde
surameli <167941245+surameli@users.noreply.github.com>
2025-11-21 14:00:54 +0300

    Fix case-sensitive import for StarBackground

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
-import StarBackground from '../components/starBackground'
+import StarBackground from '../components/StarBackground'
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
 import AboutMe from '../components/AboutMe'
